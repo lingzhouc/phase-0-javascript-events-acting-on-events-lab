2d975e329a67b371bc6e473cab3223374625d203
LingZhouChen <lzcciao@gmail.com>
2023-09-05 14:22:40 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#FF69B4";
+
+
+// moves dodger to the left
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+    }
+}
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+});
+
+// moves dodger to the right
+function moveDodgerRight () {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left < 360) {
+    dodger.style.left = `${left + 1}px`;
+    }
+}
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
